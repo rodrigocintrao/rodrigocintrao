0d36ae082f3b4fda2224d3e2c73ebeb002f14b79
Rodrigo Cintrao <rodrigo.cintrao@cwi.com.br>
2019-02-01 11:42:10 -0200

    main - cursos

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var gulp = require("gulp"),
   sourcemaps = require("gulp-sourcemaps"),
   autoprefixer = require("gulp-autoprefixer"),
   browserSync = require("browser-sync").create(),
+  concat = require("gulp-concat"),
+  uglify = require("gulp-uglify"),
   reload = browserSync.reload;
 
 //CLEAN CSS:
@@ -21,6 +23,11 @@ gulp.task("clean-fonts", function() {
   return del("./dist/fonts/**/*");
 });
 
+//CLEAN JS:
+gulp.task("clean-js", function() {
+  return del("./dist/js/main.js");
+});
+
 // SASS:
 gulp.task("sass", ["clean-css-style"], function() {
   return gulp
@@ -34,25 +41,38 @@ gulp.task("sass", ["clean-css-style"], function() {
 
 // SASS VENDOR:
 gulp.task("sass-vendor", ["clean-css-vendor"], function() {
-    return gulp
-      .src("./scss/vendor/vendor.scss")
-      .pipe(sourcemaps.init())
-      .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
-      .pipe(autoprefixer({ browsers: "last 3 versions" }))
-      .pipe(sourcemaps.write())
-      .pipe(gulp.dest("./dist/css/"));
+  return gulp
+    .src("./scss/vendor/vendor.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(autoprefixer({ browsers: "last 3 versions" }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("./dist/css/"));
 });
 
-gulp.task("copy", ["clean-fonts"], function() {
-  gulp.src([
-    "node_modules/@fortawesome/fontawesome-free/webfonts/**/*"
-  ])
-  .pipe(gulp.dest("./dist/fonts/"));
+gulp.task("copy-fontawesome", ["clean-fonts"], function() {
+  gulp
+    .src(["node_modules/@fortawesome/fontawesome-free/webfonts/**/*"])
+    .pipe(gulp.dest("./dist/fonts/"));
+});
+
+gulp.task("copy-jquery", ["clean-fonts"], function() {
+  gulp
+    .src(["node_modules/jquery/dist/jquery.min.js"])
+    .pipe(gulp.dest("./dist/js/"));
+});
+
+gulp.task("uglify", ["clean-js"], function() {
+  return gulp
+    .src("js/**/*.js")
+    .pipe(uglify())
+    .pipe(concat("main.js"))
+    .pipe(gulp.dest("./dist/js/"));
 });
 
 // WATCH:
 gulp.task("watch", function() {
-  return gulp.watch(["./scss/**/*.scss"], ["sass"]);
+  return gulp.watch(["./scss/**/*.scss", "./js/**/*.js"], ["sass", "uglify"]);
 });
 
 //SERVE
@@ -70,7 +90,9 @@ gulp.task("serve", function() {
 gulp.task("default", [
   "sass",
   "sass-vendor",
-  "copy",
+  "copy-fontawesome",
+  "copy-jquery",
+  "uglify",
   "watch",
   "serve"
 ]);
